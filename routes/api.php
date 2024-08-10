1fa93b1dbde8f02790b0bed80226f4daa1a3ba41
chinocchio <chinonoble07@gmail.com>
2024-08-10 22:09:04 +0800

    [Added API]
    - User API
    - Linked Subjects

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\ApiSubjectController;
+use App\Http\Controllers\Api\ApiInstructorsController;
+use App\Http\Controllers\Api\ApiLinkedSubjectsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('subs', ApiSubjectController::class);
+Route::apiResource('instructors', ApiInstructorsController::class);
+Route::apiResource('linkedSubjects', ApiLinkedSubjectsController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
